96f43aebf10e3ac5d5b7157666fb0599567cdf8c
ftharsh <hariharshvardhan22@gmail.com>
2024-06-14 12:44:17 +0530

    added navbar content

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export function Navbar() {
   return (
     <div>
-      <h1>navbar</h1>
+      <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center border-b dark:border-gray-700 border-gray-200">
+        <div className="flex justify-between items-center space-x-5 w-screen">
+          <Link to="/">
+            <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-500 dark:text-gray-900"></p>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
